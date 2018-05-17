18b30e57268d2935ebd11e7e443c05b68df9c788
lamdv <lam.dv@live.com>
2018-05-17 18:32:39 +0700

    May 17, 2018

--- inst/python/query_script/_filter_unique_id_script.py
+++ inst/python/query_script/_filter_unique_id_script.py
@@ -3,24 +3,42 @@
 
 # In[1]:
 
+# Import requirements:
+ 
 import os
 import sys
 sys.path.append('inst/python')
 import helper
 import query
 import csv
+from multiprocessing import Pool
+from multiprocessing.dummy import Pool as ThreadPool 
 
 
-# In[2]:
+# In[]:
 
-
+# Configuration:
+num_of_thread = 2
 path = 'data/filtered/'
 # set = ['G1_clustered_genes_annotations.tab','G2_clustered_genes_annotations.tab','G5_clustered_genes_annotations.tab','G6_clustered_genes_annotations.tab']
 set = ['G1_clustered_genes_annotations.tab']
 
+# In[2]:
+
+# Support functions:
+def get_kegg_id(key):
+    try:
+       return {'KEGG_id': key, 'Definition':query.query('kegg', [key])[0]['Definition']}
+    except:
+            print(key)
+            return None   
+
+# In[3]:
+
+
+# Get a list of unique gene
 unique_gene_list = []
 for data in set:
-    gene_list = []
     with open(path + data, newline='') as input_file:
         dictReader = csv.DictReader(input_file, delimiter = "\t")
         for line in dictReader:
@@ -30,25 +48,24 @@ for data in set:
                 if not(kegg_id in unique_gene_list): unique_gene_list.append(kegg_id)
             for kegg_id in line['KEGG_pathway'].split('|'):
                 if not(kegg_id in unique_gene_list): unique_gene_list.append(kegg_id)
-        print(len(unique_gene_list))
-        print(len(gene_list))
-#     filtered_list = list(filter(lambda gene: 750<int(gene['#gene'].split('_')[3]), gene_list))
-#     print('%d: removed %d genes which length < 750 from %d genes' % (len(filtered_list), len(gene_list)-len(filtered_list), len(gene_list)))
-#     filtered_set.append(filtered_list)
-
-# In[3]:
 
 
+# In[]:
 
+
+# Query from unique list
+# Using multiple (4) threads
+pool = ThreadPool(num_of_thread)
+unique_return = pool.map(get_kegg_id, unique_gene_list)
 
 # In[4]:
 
 
-for i in range(3):
-    if not os.path.exists(path+'filtered/'):
-        os.makedirs(path+'filtered/')
-    with open(path+'filtered/'+set[i], 'w',newline = '') as filtered:
-        dictWriter = csv.DictWriter(filtered, delimiter = '\t', fieldnames=filtered_set[i][0].keys())
-        dictWriter.writeheader()
-        dictWriter.writerows(filtered_set[i])
-
+path = 'data/filtered/'
+if not os.path.exists(path+'kegg/'):
+    os.makedirs(path+'kegg/')
+with open(path+'kegg/kegg_raw.tab', 'w',newline = '') as output_file:
+    headers = ['KEGG_id', 'Definition']
+    dictWriter = csv.DictWriter(output_file, delimiter = '\t')
+    dictWriter.writeheader()
+    dictWriter.writerows(unique_return)
